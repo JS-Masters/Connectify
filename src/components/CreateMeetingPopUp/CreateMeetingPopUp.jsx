5b111a7aec13ddc02941e9b2f6823ab63c9dff3d
loneCapricorn <mehmed.yunuz.a55@learn.telerikacademy.com>
2024-03-19 02:06:10 +0200

    Add Pop-up calendar style

--- src/components/CreateMeetingPopUp/CreateMeetingPopUp.jsx
+++ src/components/CreateMeetingPopUp/CreateMeetingPopUp.jsx
@@ -1,19 +1,22 @@
-import { Button } from "@chakra-ui/react";
+import PropTypes from "prop-types";
+import { Box, Button, Heading } from "@chakra-ui/react";
 import Popup from "reactjs-popup";
-import Sidebar from "../../pages/Calendar/CalendarSidebar";
 import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionPlugin from '@fullcalendar/interaction'
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { v4 } from "uuid";
-import { createDyteMeeting, createMeetingInDb, deleteMeeting, getMeetingsByTeamId } from "../../services/meeting.services";
-
-
+import {
+  createDyteMeeting,
+  createMeetingInDb,
+  deleteMeeting,
+  getMeetingsByTeamId,
+} from "../../services/meeting.services";
+import "./CreateMeetingPopUp.css";
 
 const CreateMeetingPopUp = ({ teamName }) => {
-
   const [weekendsVisible, setWeekendsVisible] = useState(true);
   const [currentEvents, setCurrentEvents] = useState([]);
   const [teamMeetings, setTeamMeetings] = useState([]);
@@ -21,21 +24,23 @@ const CreateMeetingPopUp = ({ teamName }) => {
   const { teamId } = useParams();
 
   useEffect(() => {
-    getMeetingsByTeamId(teamId)
-    .then((meetings) => {
-      if(meetings) {
-        const dateUpdatedMeetings = Object.values(meetings) .map((meeting) => {
-          return {...meeting, start: new Date(meeting.start), end: new Date(meeting.end)}
-        })
+    getMeetingsByTeamId(teamId).then((meetings) => {
+      if (meetings) {
+        const dateUpdatedMeetings = Object.values(meetings).map((meeting) => {
+          return {
+            ...meeting,
+            start: new Date(meeting.start),
+            end: new Date(meeting.end),
+          };
+        });
 
         setTeamMeetings(dateUpdatedMeetings);
       }
-    })
+    });
   }, [teamId, newMeetingCreated]);
 
-
   const handleWeekendsToggle = () => {
-    setWeekendsVisible(!weekendsVisible)
+    setWeekendsVisible(!weekendsVisible);
   };
 
   const handlePopUpClose = (close) => {
@@ -43,10 +48,10 @@ const CreateMeetingPopUp = ({ teamName }) => {
   };
 
   const handleDateSelect = (selectInfo) => {
-    let title = prompt('Please enter a new title for your event')
-    let calendarApi = selectInfo.view.calendar
+    let title = prompt("Please enter a new title for your event");
+    let calendarApi = selectInfo.view.calendar;
 
-    calendarApi.unselect() // clear date selection
+    calendarApi.unselect(); // clear date selection
 
     if (title) {
       calendarApi.addEvent({
@@ -54,21 +59,20 @@ const CreateMeetingPopUp = ({ teamName }) => {
         title,
         start: selectInfo.startStr,
         end: selectInfo.endStr,
-        allDay: selectInfo.allDay
-      })
+        allDay: selectInfo.allDay,
+      });
     }
   };
 
   const handleEvents = (events) => {
-    setCurrentEvents(events)
+    setCurrentEvents(events);
   };
 
   const createMeeting = (meetingId, title, start, end) => {
-    createMeetingInDb(meetingId, title, start, end, teamId)
-    .then(() => {
+    createMeetingInDb(meetingId, title, start, end, teamId).then(() => {
       createDyteMeeting(meetingId, teamId);
-      setNewMeetingCreated(!newMeetingCreated)
-    })
+      setNewMeetingCreated(!newMeetingCreated);
+    });
   };
 
   const handleDeleteMeeting = (event) => {
@@ -83,68 +87,81 @@ const CreateMeetingPopUp = ({ teamName }) => {
         <i>{eventInfo.event.title}</i>
         {/* <i>{JSON.stringify(eventInfo.event.start)}</i> */}
         <br />
-        {!(teamMeetings.find((meeting) => meeting.id === eventInfo.event.id)) &&
-          <button style={{ border: '1px solid black', padding: '3px' }} onClick={() => createMeeting(eventInfo.event.id, eventInfo.event.title, eventInfo.event.start, eventInfo.event.end)}>Create Meeting</button>}
-        <button style={{ border: '1px solid black', padding: '3px' }} onClick={() => handleDeleteMeeting(eventInfo.event)}>Delete</button>
+        {!teamMeetings.find((meeting) => meeting.id === eventInfo.event.id) && (
+          <button
+            style={{ border: "1px solid black", padding: "3px" }}
+            onClick={() =>
+              createMeeting(
+                eventInfo.event.id,
+                eventInfo.event.title,
+                eventInfo.event.start,
+                eventInfo.event.end
+              )
+            }
+          >
+            Create Meeting
+          </button>
+        )}
+        <button
+          style={{ border: "1px solid black", padding: "3px" }}
+          onClick={() => handleDeleteMeeting(eventInfo.event)}
+        >
+          Delete
+        </button>
       </>
     );
   };
 
   return (
-    <Popup trigger=
-      {<Button mb='20px'>Meetings Calendar</Button>}
-      modal nested>
-      {
-        close => (
-          <div className='modal'>
-            <Button style={{ float: "right", margin: "15px" }} onClick=
-              {() => handlePopUpClose(close)}>
-              X
-            </Button>
-            <div style={{ width: '1100px', height: '800px', border: '2px solid black', backgroundColor: 'bisque' }} className='content'>
-              <h1 style={{ textAlign: 'center', fontSize: '24px' }}>{teamName}</h1>
-              <div className='demo-app'>
-                {/* <Sidebar
-                  weekendsVisible={weekendsVisible}
-                  handleWeekendsToggle={handleWeekendsToggle}
-                  currentEvents={currentEvents}
-                /> */}
-                <div id="pop-up-calendar" className='demo-app-main' >
-                  <FullCalendar
-                  height='650px'
-                    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-                    headerToolbar={{
-                      left: 'prev,next today',
-                      center: 'title',
-                      right: 'dayGridMonth,timeGridWeek,timeGridDay'
-                    }}
-                    initialView='timeGridWeek'
-                    editable={true}
-                    selectable={true}
-                    selectMirror={true}
-                    dayMaxEvents={true}
-                    weekends={weekendsVisible}
-                    initialEvents={teamMeetings} 
-                    select={handleDateSelect}
-                    eventContent={renderEventContent}
+    <Popup trigger={<Button mb="20px">Meetings Calendar</Button>} modal nested>
+      {(close) => (
+        <Box id="pop-up-calendar" minW="1000px">
+          <Button
+            float="right"
+            colorScheme="transparent"
+            color="red"
+            onClick={() => handlePopUpClose(close)}
+          >
+            X
+          </Button>
 
-                    // eventClick={handleEventClick}
+          <Heading as="h3" m="10px" color="cyan">
+            {teamName}
+          </Heading>
+          <FullCalendar
+            height="650px"
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay",
+            }}
+            initialView="timeGridWeek"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            weekends={weekendsVisible}
+            initialEvents={teamMeetings}
+            select={handleDateSelect}
+            eventContent={renderEventContent}
+            // eventClick={handleEventClick}
 
-                    eventsSet={handleEvents} // called after events are initialized/added/changed/removed
-                  /* you can update a remote database when these fire:
+            eventsSet={handleEvents} // called after events are initialized/added/changed/removed
+            /* you can update a remote database when these fire:
                   eventAdd={function(){}}
                   eventChange={function(){}}
                   eventRemove={function(){}}
                   */
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        )
-      }
+          />
+        </Box>
+      )}
     </Popup>
   );
+};
+
+CreateMeetingPopUp.propTypes = {
+  teamName: PropTypes.string.isRequired,
 };
 
 export default CreateMeetingPopUp;
